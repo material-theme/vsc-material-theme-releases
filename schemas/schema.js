55498f75ef031cb7f8a81c0736b13535707fd5d1
LasaleFamine <info@godev.space>
2018-12-22 13:00:50 +0100

    init Sanity

---
+++ schemas/schema.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-unresolved */
+// First, we must import the schema creator
+import createSchema from 'part:@sanity/base/schema-creator';
+
+// Then import schema types from any plugins that might expose them
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+
+// We import object and document schemas
+import blockContent from './block-content';
+import lightBlockContent from './light-block-content';
+import release from './release';
+
+// Then we give our schema to the builder and provide the result to Sanity
+export default createSchema({
+	// We name our schema
+	name: 'default',
+	// Then proceed to concatenate our our document type
+	// to the ones provided by any plugins that are installed
+	types: schemaTypes.concat([
+		// The following are document types which will appear
+		// in the studio.
+		release,
+		// When added to this list, object types can be used as
+		// { type: 'typename' } in other document schemas
+		blockContent,
+		lightBlockContent
+	])
+});
